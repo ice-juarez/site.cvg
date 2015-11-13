91b0c33f07597df4065f6a3611b92081f82311dc
ice.juarez <ice.juarez@hotmail.com>
2015-11-13 02:04:21 -0600

    Add line

--- README.md
+++
@@ -1,2 +0,0 @@
-# site.cvg
-Projecto para el sitio CVG
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php 
+	require_once "rcs/Autoload.php";
+	Config\Autoload::run();
+
+	$est = new Models\Estudiante();
+	$est->hola();
+/*$est->set("id", 1);
+$datos = $est->view();
+print $datos['nombre'];*/
+//Ingresando nueva linea
+?>
